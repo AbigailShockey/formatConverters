5beabbed4390fae247c0cacf4ab448e78865b249
AbigailShockey <abbashockey@gmail.com>
2019-12-10 14:46:09 -0600

    Add comments

--- abricatePresenceAbsence.py
+++ abricatePresenceAbsence.py
@@ -26,16 +26,21 @@ try:
 except (AttributeError, TypeError) as err:
     out = os.getcwd()
 
-# Set pandas display options (for printing)
-pd.options.display.max_rows = 10
+# Ssoptions.display.max_rows = 10
 pd.options.display.max_columns = 5
 
+# get max identity for each entry, if any
 def identity(val):
+    # convert to str
     val = list(map(str, val))
     for i in range(0,len(val)):
       vals = val[i].split(";")
+
+      # if AR gene absent, set to 0
       if len(vals) == 1 and vals[0] == ".":
           val[i] = 0
+
+      # if AR gene(s) present, get max value
       else:
           vals = list(map(float, vals))
           val[i] = max(vals)
@@ -45,22 +50,33 @@ def binary(val):
     for i in range(0,len(val)):
       if val[i] == 0:
           val[i] = 0
+      # if AR gene present, set to 1
       else:
           val[i] = 1
     return val
 
+# get path to abricate tsv file
 tsvfile = os.path.abspath(args.tsv)
+
+# get abricate tsv file handle
 tsvhandle = os.path.basename(tsvfile).split(".")[0] + "_binary.tsv"
+
+# read abricate tsv file to df
 tsv = pd.read_csv(tsvfile, sep="\t", header=0)
+
+# set path to output file
 binaytsv = os.path.join(out,tsvhandle)
 
+# get identity of each entry in the df
 for i in range(2,len(tsv.columns)):
  tsv.iloc[0:len(tsv.index),i] = identity(list(tsv.iloc[0:len(tsv.index), i]))
 
+# convert df to binary
 bin_tsv = tsv
 for i in range(2,len(bin_tsv.columns)):
     print(list(bin_tsv.iloc[0:len(bin_tsv.index), i]))
     bin_tsv.iloc[0:len(bin_tsv.index),i] = binary(list(bin_tsv.iloc[0:len(bin_tsv.index), i]))
     print(list(bin_tsv.iloc[0:len(bin_tsv.index), i]))
 
+# write binary df to file
 bin_tsv.to_csv(binaytsv, sep='\t', encoding='utf-8')
